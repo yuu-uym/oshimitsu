22749e022c01796746e47d8ca7178a6fb9b56bef
yuu-uym <uyama0816y@gmail.com>
2020-12-14 11:05:25 +0900

    Active Storageの導入

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,4 +1,8 @@
 class GenresController < ApplicationController
   def index
   end
+
+  def message_params
+    params.require(:genre).permit(:theme, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,4 @@
 class Genre < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 end
