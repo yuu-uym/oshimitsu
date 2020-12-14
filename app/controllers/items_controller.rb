56146e0ce001aa4f96afc0c45e659ffe6733c97f
yuu-uym <uyama0816y@gmail.com>
2020-12-14 16:01:11 +0900

    アイテム登録画面への遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,14 @@
 class ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :category_id, :price, :quantity_id, :status_id, :release_date, :purchase_date).merge(user_id: current_user.id, genre_id: params[:genre_id])
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -13,5 +13,4 @@ class Category < ActiveHash::Base
   ]
   include ActiveHash::Associations
   has_many :items
-  end
 end
