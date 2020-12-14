93592180996b1aea33acdc3859f8262068fa472d
yuu-uym <uyama0816y@gmail.com>
2020-12-14 15:44:04 +0900

    itemコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'genres#index'
-  resources :genres
+  resources :genres do
+    resources :items
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
