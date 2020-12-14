9ba8f73935b6bd08b9ffbe180a1443709ff0e105
yuu-uym <uyama0816y@gmail.com>
2020-12-14 15:40:07 +0900

    categoryモデル作成

---
+++ app/models/category.rb
@@ -0,0 +1,17 @@
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'グッズ' },
+    { id: 3, name: 'チケット' },
+    { id: 4, name: 'イベント' },
+    { id: 5, name: '冊子' },
+    { id: 6, name: 'DVD/Blu-ray' },
+    { id: 7, name: 'CD' },
+    { id: 8, name: '遠征費' },
+    { id: 9, name: '課金' },
+    { id: 10, name: 'その他' }
+  ]
+  include ActiveHash::Associations
+  has_many :items
+  end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :genre
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  
+  validates :genre_id, numericality: { other_than: 1 } 
 end
