1cdf65ed48770c1d27c2816c77b933dbdf2a406b
yuu-uym <uyama0816y@gmail.com>
2020-12-14 17:34:59 +0900

    アイテム保存できた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,12 @@ class ItemsController < ApplicationController
   end
   
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,8 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :status
   
   validates :genre_id, numericality: { other_than: 1 } 
+  validates :status_id, numericality: { other_than: 1 } 
 end
