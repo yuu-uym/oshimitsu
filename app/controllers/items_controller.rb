c8d37a8d698af42079b0037ff86837843bab0a98
yuu-uym <uyama0816y@gmail.com>
2020-12-15 14:26:40 +0900

    アイテム削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
+
+  def index
+    @item = Item.all
+  end
+
   def new
     @item = Item.new
   end
@@ -12,24 +17,11 @@ class ItemsController < ApplicationController
     end
   end
 
-  def edit
-    @item = Item.find(params[:id])
-    redirect_to action: :index if @item.user_id != current_user.id 
-  end
-
-  def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-       redirect_to genre_path
-    else
-      render :edit
-    end
-  end
-
   def destroy
-    @item = Item.find_by(id: params[:id])
-    @item.destroy
-    redirect_to root_path
+    @genre = Genre.find(params[:genre_id])
+    @item = @genre.items.find(params[:id])
+    if @item.destroy
+       redirect_to genre_path(@genre)
     end
   end
 
@@ -37,3 +29,5 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :category_id, :price, :quantity_id, :status_id, :release_date, :purchase_date).merge(user_id: current_user.id, genre_id: params[:genre_id])
   end
+
+end
