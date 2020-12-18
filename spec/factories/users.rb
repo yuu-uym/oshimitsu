d0ac0aff03c95eb558537454f238c7367cf88c40
yuu-uym <uyama0816y@gmail.com>
2020-12-18 11:40:16 +0900

    ユーザー新規登録のテストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.last_name }
     email                 { Faker::Internet.free_email }
-    password { Faker::Internet.password(min_length: 6) }
+    password {'1234abcd' }
   end
 end
