5a8263888a8c8a0fad8bf7710d192c1f32b40e8d
yuu-uym <uyama0816y@gmail.com>
2021-01-20 20:01:14 +0900

    model記述少しみやすくした

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
   has_many :genres
   has_many :items
+
   validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
