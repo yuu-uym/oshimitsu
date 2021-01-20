196b95108be2f66bec7a0c08bed03372eec1a24b
yuu-uym <uyama0816y@gmail.com>
2021-01-21 00:58:15 +0900

    Update user.rb
    デプロイエラー

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
   has_many :genres
   has_many :items
-
   validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
