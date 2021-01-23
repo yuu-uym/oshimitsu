b1a0d69b318d99a5341b8bdf92740311009569f2
yuu-uym <uyama0816y@gmail.com>
2021-01-23 16:45:59 +0900

    genreにカラムの追加

--- db/migrate/20201213112304_create_genres.rb
+++ db/migrate/20201213112304_create_genres.rb
@@ -3,6 +3,12 @@ class CreateGenres < ActiveRecord::Migration[6.0]
     create_table :genres do |t|
       t.string :theme,    null: false
       t.integer :set_amount
+      t.text :title1
+      t.text :url1
+      t.text :title2
+      t.text :url2
+      t.text :title2
+      t.text :url2
       t.timestamps
     end
   end
