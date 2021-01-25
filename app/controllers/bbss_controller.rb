23972ca30776c049e946870a76635a24735795ee
yuu-uym <uyama0816y@gmail.com>
2021-01-25 13:43:01 +0900

    掲示板の保存

--- app/controllers/bbss_controller.rb
+++ app/controllers/bbss_controller.rb
@@ -1,6 +1,24 @@
 class BbssController < ApplicationController
 
   def index
+    @bbss = Bbs.all
   end
 
+  def new
+    @bbs = Bbs.new
+  end
+  
+  def create
+    @bbs = Bbs.new(bbs_params)
+    if @bbs.save
+      redirect_to bbss_index_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def bbs_params
+    params.require(:bbs).permit(:title, :overview).merge(user_id: current_user.id)
+  end
 end
--- app/models/bb.rb
+++
@@ -1,2 +0,0 @@
-class Bb < ApplicationRecord
-end
